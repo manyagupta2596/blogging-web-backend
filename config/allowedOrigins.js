3b07638b03e5fffe15e79fd8e2e0307548cf4549
manya gupta <115874927+manyagupta2596@users.noreply.github.com>
2024-07-25 13:51:15 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:4200',
-  'https://gleeful-fenglisu-4c6e66.netlify.app'
+  'https://manyaguptablogs.netlify.app'
+  
 ];
 
 module.exports = allowedOrigins;
